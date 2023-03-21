99207414fa21355e76f3789c76ad90e70475d6c4
Michellezta <101204440+Michellezta@users.noreply.github.com>
2023-03-21 12:41:14 -0600

    cambios 2

---
+++ Tienda/Tienda/figuras de accion.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Figuras de accion</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery-3.6.1.js"></script>
+</head>
+<body>
+    <?php include 'menu.php' ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="jumbotron">
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+    <div class="card h-100">
+      <img src="figuras de acccion 1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Emperor Palpatine  </h5>
+        <p class="card-text">cofvfc</p>
+        <a href="index.php" class="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="figuras de acccion 2.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Juguete OBI-WAN Kenobi</h5>
+        <p class="card-text">Ahgdfgf</p>
+        <a href="index.php" class="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="figuras de acccion 3.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Heavy Assault Stormtrooper</h5>
+        <p class="card-text">Agdfgd.</p>
+        <a href="index.php" class="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+  </div>
+                        </div>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="text-center">
+        <hr>
+        2022 &copy; Jugueteria
+    </footer>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
