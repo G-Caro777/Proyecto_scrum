9900cd016636d20275bb4e4de3926076d2342729
joanelizandroochoa <114783820+joanelizandroochoa@users.noreply.github.com>
2023-04-27 18:39:32 -0700

    Add files via upload

---
+++ base_de_datos1.sql
@@ -0,0 +1,396 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-04-2023 a las 04:38:54
+-- Versión del servidor: 10.1.39-MariaDB
+-- Versión de PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `base_de_datos1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comisiones`
+--
+
+CREATE TABLE `comisiones` (
+  `id_comisiones` int(11) NOT NULL,
+  `porcentaje` int(11) NOT NULL,
+  `tipo_de_transaccion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `direcciones`
+--
+
+CREATE TABLE `direcciones` (
+  `id_direcciones` int(11) NOT NULL,
+  `pais` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `ciudad` varchar(50) NOT NULL,
+  `colonia` varchar(50) NOT NULL,
+  `calle` varchar(50) NOT NULL,
+  `codigo_postal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `envios`
+--
+
+CREATE TABLE `envios` (
+  `id_envio` int(11) NOT NULL,
+  `transaccion_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `direccion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favoritos`
+--
+
+CREATE TABLE `favoritos` (
+  `id_favoritos` int(11) NOT NULL,
+  `prenda_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ofertas`
+--
+
+CREATE TABLE `ofertas` (
+  `id_oferta` int(11) NOT NULL,
+  `prenda_id` int(11) NOT NULL,
+  `precio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prendas`
+--
+
+CREATE TABLE `prendas` (
+  `id_prenda` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `talla` varchar(3) NOT NULL,
+  `color` varchar(20) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `sexo` tinyint(1) NOT NULL,
+  `imagen` int(11) NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  `exp_fecha` date DEFAULT NULL,
+  `tipo_transaccion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitudes`
+--
+
+CREATE TABLE `solicitudes` (
+  `id_solicitud` int(11) NOT NULL,
+  `prenda_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `prenda2_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_de_pago`
+--
+
+CREATE TABLE `tipo_de_pago` (
+  `id_tipo_de_pago` int(11) NOT NULL,
+  `tipo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_de_transaccion`
+--
+
+CREATE TABLE `tipo_de_transaccion` (
+  `id_tipo_de_transaccion` int(11) NOT NULL,
+  `tipo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transaccion`
+--
+
+CREATE TABLE `transaccion` (
+  `id_transaccion` int(11) NOT NULL,
+  `tipo` tinyint(1) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `tipo_de_pago_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transaccion_prenda`
+--
+
+CREATE TABLE `transaccion_prenda` (
+  `id_transaccion_prenda` int(11) NOT NULL,
+  `prenda_id` int(11) NOT NULL,
+  `transaccion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `correo_electronico` varchar(100) NOT NULL,
+  `nickname` varchar(1000) NOT NULL,
+  `direccion_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `comisiones`
+--
+ALTER TABLE `comisiones`
+  ADD PRIMARY KEY (`id_comisiones`),
+  ADD KEY `tipo_de_transaccion_id` (`tipo_de_transaccion_id`);
+
+--
+-- Indices de la tabla `direcciones`
+--
+ALTER TABLE `direcciones`
+  ADD PRIMARY KEY (`id_direcciones`);
+
+--
+-- Indices de la tabla `envios`
+--
+ALTER TABLE `envios`
+  ADD PRIMARY KEY (`id_envio`),
+  ADD KEY `transaccion_id` (`transaccion_id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `direccion_id` (`direccion_id`);
+
+--
+-- Indices de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD PRIMARY KEY (`id_favoritos`),
+  ADD KEY `prenda_id` (`prenda_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `ofertas`
+--
+ALTER TABLE `ofertas`
+  ADD PRIMARY KEY (`id_oferta`),
+  ADD KEY `prenda_id` (`prenda_id`);
+
+--
+-- Indices de la tabla `prendas`
+--
+ALTER TABLE `prendas`
+  ADD PRIMARY KEY (`id_prenda`),
+  ADD KEY `categoria_id` (`categoria_id`),
+  ADD KEY `tipo_transaccion_id` (`tipo_transaccion_id`);
+
+--
+-- Indices de la tabla `solicitudes`
+--
+ALTER TABLE `solicitudes`
+  ADD PRIMARY KEY (`id_solicitud`),
+  ADD KEY `prenda_id` (`prenda_id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `usuario_id_2` (`usuario_id`),
+  ADD KEY `prenda_id_2` (`prenda_id`),
+  ADD KEY `prenda2_id` (`prenda2_id`);
+
+--
+-- Indices de la tabla `tipo_de_pago`
+--
+ALTER TABLE `tipo_de_pago`
+  ADD PRIMARY KEY (`id_tipo_de_pago`);
+
+--
+-- Indices de la tabla `tipo_de_transaccion`
+--
+ALTER TABLE `tipo_de_transaccion`
+  ADD PRIMARY KEY (`id_tipo_de_transaccion`);
+
+--
+-- Indices de la tabla `transaccion`
+--
+ALTER TABLE `transaccion`
+  ADD PRIMARY KEY (`id_transaccion`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `tipo_de_pago_id` (`tipo_de_pago_id`);
+
+--
+-- Indices de la tabla `transaccion_prenda`
+--
+ALTER TABLE `transaccion_prenda`
+  ADD PRIMARY KEY (`id_transaccion_prenda`),
+  ADD KEY `prenda_id` (`prenda_id`),
+  ADD KEY `transaccion_id` (`transaccion_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `direccion_id` (`direccion_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `comisiones`
+--
+ALTER TABLE `comisiones`
+  MODIFY `id_comisiones` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `direcciones`
+--
+ALTER TABLE `direcciones`
+  MODIFY `id_direcciones` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `envios`
+--
+ALTER TABLE `envios`
+  MODIFY `id_envio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  MODIFY `id_favoritos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ofertas`
+--
+ALTER TABLE `ofertas`
+  MODIFY `id_oferta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `prendas`
+--
+ALTER TABLE `prendas`
+  MODIFY `id_prenda` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `solicitudes`
+--
+ALTER TABLE `solicitudes`
+  MODIFY `id_solicitud` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_de_pago`
+--
+ALTER TABLE `tipo_de_pago`
+  MODIFY `id_tipo_de_pago` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_de_transaccion`
+--
+ALTER TABLE `tipo_de_transaccion`
+  MODIFY `id_tipo_de_transaccion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `transaccion`
+--
+ALTER TABLE `transaccion`
+  MODIFY `id_transaccion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `transaccion_prenda`
+--
+ALTER TABLE `transaccion_prenda`
+  MODIFY `id_transaccion_prenda` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD CONSTRAINT `favoritos_prenda` FOREIGN KEY (`prenda_id`) REFERENCES `prendas` (`id_prenda`);
+
+--
+-- Filtros para la tabla `prendas`
+--
+ALTER TABLE `prendas`
+  ADD CONSTRAINT `categoria_de_prenda` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id_categoria`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
